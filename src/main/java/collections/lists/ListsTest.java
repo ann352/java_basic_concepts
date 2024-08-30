9b2ded75f26356149cd9992e44a6560cf35d77b4
ann352 <anna.woznickaaa@gmail.com>
2024-08-30 14:26:40 +0200

    FX-01 aded some infos about lists

--- src/main/java/collections/lists/ListsTest.java
+++ src/main/java/collections/lists/ListsTest.java
@@ -1,2 +1,98 @@
-package collections.lists;public class Lists {
+package collections.lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListsTest {
+
+    //the most popular type of collection,allow to add any number of data and remember the order of added items
+    //they allow to refer to them using indexes, we don't have to specify an initial size for
+    //it will automatically grow if you add an item to it and shrink if we remove it
+    // List - it is interface - we can't create object List, but we can create it's implementation
+    // e.g. LinkedList, or ArrayList
+
+    public static void main(String[] args) {
+
+        //methods used in lists, common for all collections
+
+        List<Object> names = new ArrayList<>();
+
+        //1.add()
+        names.add("Ania");
+        names.add("Hania");
+        names.add("Basia");
+        names.add("Kuba");
+        names.add("Kuba");
+
+        //2.size()
+        int size = names.size();
+        System.out.println(size);
+
+        //3.isEmpty()
+        System.out.println(names.isEmpty());
+
+        //4.contains()
+        System.out.println(names.contains("Ania"));
+
+        //5.remove()
+        System.out.println(names.remove("Ania"));
+        System.out.println(names.contains("Ania"));
+
+        //additional methods - specific only for in lists - that operate on indexes
+
+        //6. E - type of element, e.g. E get(int index)
+        System.out.println(names.get(0));
+
+        //7. indexOf()
+        System.out.println(names.indexOf("Hania")); //if element doesn't exist it returns -1,
+
+        //8. void add(int index, E element)
+        names.add(1, "Tomasz");
+        System.out.println(names);
+
+        //9.set()  replace element with given index, returns the element that was replaced (old value)
+        names.set(1, "Kuba");
+        System.out.println(names);
+
+        //printing , iterating over lists
+
+        //1. we can directly print list in println, because lists have own toString()
+        System.out.println(names);
+
+        //2.in loop - when we wan't to display something in different way
+
+        //for
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+
+        //for-each
+        for (Object name : names) {
+            System.out.println(name);
+        }
+
+        //removing elements from list in loop
+        //we shouldn't use for-each loop - it is used usually used to view elements, it can cause a exception
+
+        for (Object element : names) {
+            if (element.equals("Kuba")) {
+                names.remove(element);
+                break; //we have to use break here
+            }
+        }
+        System.out.println(names);
+
+        //proper way: we can use for-loop
+
+        for(int i =0; i < names.size() ; i++){
+            if (names.get(i).equals("Kuba")) {
+                names.remove(i);
+                i--; //we have to undo iterator
+            }
+        }
+        System.out.println(names);
+    }
+
+
 }
+
