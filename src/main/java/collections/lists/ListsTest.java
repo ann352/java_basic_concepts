2b5ce8ff5571a8f3e2d5664f447cab49dcfd0451
ann352 <anna.woznickaaa@gmail.com>
2024-09-10 15:13:46 +0200

    FX-01 aded Comparator for sorting elements in lists

--- src/main/java/collections/lists/ListsTest.java
+++ src/main/java/collections/lists/ListsTest.java
@@ -1,6 +1,8 @@
 package collections.lists;
 
+import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ListsTest {
 
@@ -75,12 +77,12 @@ public class ListsTest {
         //with 'new String[0]' notation - we say Java - type of result table
 
         //and also array to list, with below static method
-        String[] namesArray = {"Adam","Ewa","Kacper"};
+        String[] namesArray = {"Adam", "Ewa", "Kacper"};
         System.out.println("List from Array" + Arrays.asList(namesArray));
 
         //we can also crate list like that:
         List<String> list = Arrays.asList("Ela", "Hela");
-        System.out.println("New list: " + list) ;
+        System.out.println("New list: " + list);
         //the list that is created after using Arrays.asList
         //has a specific implementation that does not allow adding new elements or removing them
         //when we conduct that operation we get UnsupportedOperationException
@@ -90,7 +92,7 @@ public class ListsTest {
 
         //iterator()
         Iterator<String> iterator = names.iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             System.out.println(iterator.next().toUpperCase());
         }
 
@@ -98,7 +100,7 @@ public class ListsTest {
 
         // E - type of element, e.g. E get(int index)
         System.out.println("First element of 'names': " + names.get(0)); //first element
-        System.out.println("Last element of 'names': " + names.get(names.size() -1));//last element
+        System.out.println("Last element of 'names': " + names.get(names.size() - 1));//last element
 
         // indexOf() -> it returns first index of element
         System.out.println("First index of ania " + names.indexOf("Ania")); //if element doesn't exist it returns -1,
@@ -133,11 +135,11 @@ public class ListsTest {
 
         //iterator
         Iterator<String> namesIterator = names.iterator();
-        while (namesIterator.hasNext()){
+        while (namesIterator.hasNext()) {
             System.out.println(namesIterator.next());
         }
 
-        //removing elements from list in loop
+        //removing elements
         //we shouldn't use for-each loop - it is used usually used to view elements, it can cause an exception
         //to avoid error we van add here 'break' but in for-each we can remove only one value
         //generally we have problems in that loop because it is constantly smaller and we always go to the next element
@@ -150,15 +152,60 @@ public class ListsTest {
         }
         System.out.println(names);
 
-        //proper way: we can use for-loop
+        //a little better solution is for-loop, but we have to remeber about decrease
 
-        for(int i =0; i < names.size() ; i++){
+        for (int i = 0; i < names.size(); i++) {
             if (names.get(i).equals("Kuba")) {
                 names.remove(i);
                 i--; //we have to undo iterator
             }
         }
         System.out.println(names);
+
+        //googd solutuon is removing elements with iterator
+        List<String> names2 = new ArrayList<>(
+                Arrays.asList("Adam", "Ewa", "Kacper")
+        );
+        Iterator<String> iterator3 = names2.iterator();
+        while (iterator3.hasNext()) {
+            String currentName = iterator3.next();
+            if (currentName.length() > 3 || currentName.startsWith("E")) {
+                iterator3.remove();
+            }
+        }
+        System.out.println(names2);
+
+        //we can also create a new list without specified elements
+
+        //foreach
+        List<String> polishNames = Arrays.asList("Tomasz", "Ala", "Tadeusz", "Michał");
+        List<String> polishNamesWithoutTletter = new ArrayList<>();
+        for (String name : names) {
+            if (!name.startsWith("T")) {
+                polishNamesWithoutTletter.add(name);
+
+            }
+        }
+        System.out.println(polishNamesWithoutTletter);
+
+        // for
+        List<String> names3 = Arrays.asList("Ala", "Adam", "Kacper", "Kacper");
+        List<String> namesFiltered = new ArrayList<>();
+        for (int i = 0; i < names3.size(); i++) {
+            if (!names3.get(i).startsWith("Kacper")) {
+                namesFiltered.add(names3.get(i));
+            }
+        }
+        System.out.println(namesFiltered);
+
+
+        //stream
+        List<String> femineNames = Arrays.asList("Meg", "Merry", "Eva", "Kimberly", "Jane");
+
+        List<String> filteredFemineNames = femineNames.stream().
+                filter(name -> !name.startsWith("M")).
+                collect(Collectors.toList());
+        System.out.println(filteredFemineNames);
 
         //listiterator() -> it returns iterator that can going to previous / next
         ListIterator<String> stringListIterator = names.listIterator();
@@ -167,8 +214,34 @@ public class ListsTest {
         names.sort(Comparator.reverseOrder());
         System.out.println(names);
 
-    //List subList(int fromIndex, int toIndex)
-        System.out.println(names.subList(1,3));
+        //List subList(int fromIndex, int toIndex)
+        System.out.println(names.subList(1, 3));
+
+        //removing duplicates
+        //good idea is put elements to linkedHashList<> in case to save order and letter pack those unique elements to list
+
+        List<String> words = Arrays.asList("home", "bubble", "tree", "good", "sun","bubble");
+        LinkedHashSet<String> uniqueWords = new LinkedHashSet<>(words);
+        System.out.println(new ArrayList<>(uniqueWords));
+
+        //for deduplicating objects in our class we should implement the equals and hashCode methods
+
+        //sorting
+        //when sorting basic objects like String, Integer, Double it is enough to use the static sort() method from the Collections class
+        //we sort elements in old list
+        Collections.sort(words);
+        System.out.println(words);
+
+        //other classes will be sorted according to their implementation of the int compareTo(T o) method
+        List<String> sweets = Arrays.asList("lolipop", "ice-cream", "bon-bon", "bisquits", "cake");
+        sweets.sort(new StringLenghtComparator()); //here we have to put new object of comparator
+        System.out.println(sweets);
+        //or option with lambda - here reverse order
+        sweets.sort((s1,s2)->s2.length() - s1.length());
+        System.out.println(sweets);
+        //or easier, with method reference -> Comparator.comparingInt(String::length)
+
+
     }
 }
 
